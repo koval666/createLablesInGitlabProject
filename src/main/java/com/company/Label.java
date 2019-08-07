4410df2adfef6de60025f62882768ed03bc64a56
Dmitriy Kovalenko <kovalenko-d@haulmont.com>
2019-08-07 13:05:12 +0400

    initial commit

---
+++ src/main/java/com/company/Label.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * @author kovalenko-d
+ * @version $
+ */
+public class Label {
+    private String name;
+    private String color;
+
+    public Label(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    static List<Label> labels = Arrays.asList(
+            new Label("Submitted", "#428BCA"),
+            new Label("Resubmitted", "#0033CC"),
+            new Label("Need to fix", "#D9534F"),
+            new Label("In review", "#FED74A"),
+            new Label("Approved", "#5CB85C"));
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Project id:");
+        String projectId = buffer.readLine();
+        System.out.println("Token:");
+        String token = buffer.readLine();
+
+        for (Label label : labels) {
+            HttpResponse httpResponse = Request.Post("https://git.haulmont.com/api/v4/projects/" + projectId + "/labels")
+                    .addHeader("PRIVATE-TOKEN", token)
+                    .bodyForm(Form.form().add("name", label.getName())
+                            .add("color", label.getColor()).build())
+                    .execute()
+                    .returnResponse();
+            System.out.println(IOUtils.toString(httpResponse.getEntity().getContent()));
+        }
+
+        System.out.println("Press Enter to exit");
+        buffer.readLine();
+        buffer.close();
+    }
+}
